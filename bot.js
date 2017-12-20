a49dbeea82e0ad53622404b7aa93d1eec5509db8
Pixelstyx <jsparxs@gmail.com>
2017-12-20 10:05:10 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,12 +10,6 @@ logger.add(logger.transports.Console, {
 });
 logger.level = 'debug';
 
-var http = require('http');
-var port = process.env.PORT || 3000;
-http.listen(port, function () {
-  console.log('My server is now running on http://localhost:' + port);
-});
-
 // Initialize Discord Bot
 var bot = new Discord.Client({
    token: auth.token,
